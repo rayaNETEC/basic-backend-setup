32fc339f2fe6a9884c8cc40a096d5e8475a8721b
Rayaninha <salesrayane11@gmail.com>
2024-08-16 11:45:12 -0300

    chore: docs

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,34 @@
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
 import { env } from "~/env";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Basic Backend Setup",
+      description: "",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server running!");
