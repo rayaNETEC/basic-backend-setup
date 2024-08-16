f5b1b103fd6da3b704b5fa0abc14baa36514940d
Rayaninha <salesrayane11@gmail.com>
2024-08-16 11:43:27 -0300

    chore: initial server & aliases of imports

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .union([z.literal("dev"), z.literal("production"), z.literal("test")])
+    .default("dev"),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.log("Invalid enviroment variables.", _env.error.format());
+
+  throw new Error("Invalid enviroment variables.");
+}
+
+export const env = _env.data;
