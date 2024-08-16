5391291436d5798ff136013088ba6dba3df4d3c6
Rayaninha <salesrayane11@gmail.com>
2024-08-16 11:50:53 -0300

    chore: simple import sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  { plugins: { 'simple-import-sort': simpleImportSort } },
+  {
+    rules: {
+      'simple-import-sort/imports': 'error',
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
